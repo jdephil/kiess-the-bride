973da00c298c6ca8351aa627a3632c398586075f
Jennifer De Phillips <jennifer@thinknimble.com>
2023-08-17 20:10:58 +0100

    added axios instance

---
+++ client/src/services/axios-instance.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError } from 'axios'
+import { localStoreManager } from 'src/utils/local-store-manager'
+import { getCookie } from 'src/utils/get-cookie'
+
+const baseURL = `${window.location.protocol}//${window.location.host}`
+
+export const axiosInstance = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const token = localStoreManager.token.get()
+    if (token) {
+      const authHeader = `Token ${token}`
+      const csrfToken = getCookie('csrftoken')
+      if (config.headers) {
+        config.headers.Authorization = authHeader
+        config.headers['X-CSRFToken'] = csrfToken
+      } else {
+        config.headers = new axios.AxiosHeaders({
+          Authorization: authHeader,
+          'X-CSRFToken': csrfToken,
+        })
+      }
+    }
+    return { ...config }
+  },
+  (error: Error | AxiosError) => {
+    return Promise.reject(error)
+  },
+)
