f27a79c03c38aa9a1429e13acfb62fa3e44c282d
Jennifer De Phillips <jennifer@thinknimble.com>
2023-10-13 13:17:30 -0400

    removed sign up and auth

--- client/src/utils/routes.tsx
+++ client/src/utils/routes.tsx
@@ -1,30 +1,13 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { Home, Layout, LogIn, SignUp } from 'src/pages'
-import { AppOrAuth } from 'src/pages/app-or-auth'
-import { useAuth } from 'src/stores/auth'
-
-const PrivateRoutes = () => {
-  const token = useAuth.use.token()
-  const isAuth = Boolean(token)
-  return isAuth ? (
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/private" element={<div>Hello from private</div>} />
-    </Routes>
-  ) : (
-    <Navigate to="/log-in" />
-  )
-}
+import { Home, Layout, LogIn } from 'src/pages'
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<AppOrAuth />} />
         <Route path="/log-in" element={<LogIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/*" element={<PrivateRoutes />} />
+        <Route path="/home" element={<Home />} />
       </Route>
     </Routes>
   )
