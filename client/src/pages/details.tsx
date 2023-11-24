6ffd059d23c60a0aad2430a2ef1082a2bc715ef2
Jennifer De Phillips <jennifer@thinknimble.com>
2023-11-24 17:15:55 -0500

    left forest placed

--- client/src/pages/details.tsx
+++ client/src/pages/details.tsx
@@ -1,3 +1,10 @@
+import forestLeft from 'src/assets/images/nightime_forest_left.png'
 export const Details = () => {
-  return <></>
+  return (
+    <div className="bg-wedding-green grid h-screen grid-cols-12 grid-rows-6">
+      <div className="col-start-1 col-end-7 row-start-3 row-end-7  flex">
+        <img className="mb-5 ml-5" src={forestLeft} />
+      </div>
+    </div>
+  )
 }
