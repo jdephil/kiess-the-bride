fbec3f07c11a47dcb21a561c5c887d093affe945
Jennifer De Phillips <jennifer@thinknimble.com>
2023-12-22 18:14:43 -0500

    indiviudal stars left

--- client/src/pages/details.tsx
+++ client/src/pages/details.tsx
@@ -1,3 +1,4 @@
+import { Heading } from 'src/components/heading'
 import forestLeft from 'src/assets/images/nightime_forest_left.png'
 import forestRight from 'src/assets/images/nighttime_forest_right.png'
 import starsLeft from 'src/assets/images/left_stars.png'
@@ -8,16 +9,7 @@ export const Details = () => {
   return (
     <main className="flex min-h-screen flex-col justify-center">
       <div className="relative h-screen overflow-scroll bg-wedding-green">
-        <div className="fixed left-0 ">
-          <img className="ml-10 mt-12 w-96" src={starsLeft} />
-        </div>
-        <div className="fixed right-0">
-          <img className=" mt-12 w-96" src={starsRight} />
-        </div>
-
-        <div className="pl-moon pb-12">
-          <img className="w-36" src={moon} />
-        </div>
+        <Heading />
         <div className="">
           <div className=" text-center text-white">
             <p className="">Chris & Jen</p>
