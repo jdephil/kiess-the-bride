f3797413fabd66b2afde0acccad9a74c978f1299
Jennifer De Phillips <jennifer@thinknimble.com>
2024-06-08 00:04:12 -0400

    shuttle info:

--- client/src/components/transportation.tsx
+++ client/src/components/transportation.tsx
@@ -3,9 +3,12 @@ export default function Transportation() {
     <div id="transportation" className="mx-5 pt-24 md:mx-0">
       <div className="font-abel  text-4xl">Transportation</div>
       <div>
+        <p className="mt-10">We will have a shuttle from Suites at Silver Tree.</p>
         <p className="mt-10">
-          We will have a shuttle from Suites at Silver Tree and other stops. Check back closer to
-          the wedding date for a full schedule.
+          5:15 - 6:00 Shuttles back and forth between Suites at Silver Tree and wedding venue
+        </p>
+        <p className="mt-10">
+          11:00 - 12:50 Shuttles back and forth between venue and Suites at Silver Tree
         </p>
       </div>
     </div>
